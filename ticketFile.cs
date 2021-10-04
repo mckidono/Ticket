9e0be8343ec2a2572d0d601d1a49fa48d86ed7a3
mckidono <dmckinney6@my.wctc.edu>
2021-10-03 20:54:41 -0500

    Adding NLog but NLogBuilder doesnt work, I will just ask in class

--- ticketFile.cs
+++ ticketFile.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using NLog.Web;
 
-
-namespace ticketLibrary
+namespace Ticket
 {
     public class ticketFile
     {
+        public string filePath { get; set;}
+        public List<ticket> Tickets {get; set;}
+//        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
     }
 }
-        
